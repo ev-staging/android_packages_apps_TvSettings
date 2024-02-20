be70e85bb81a60727141c304fc8a4d252d1bdd58
Timi Rautamäki <timi.rautamaki@gmail.com>
2024-02-20 16:44:20 +0000

    TvSettings: fix crash on developer options
    If the mUnavailable variable is true, onResume() returns early, and
    mConnectivityManager.registerNetworkCallback is not ran. Later,
    onPause() crashes when scrolling past the page.
    
    Test: manual
    Change-Id: Iaa8ddc52de04b9392477be2ba557770d1f05e0af

--- Settings/src/com/android/tv/settings/system/development/DevelopmentFragment.java
+++ Settings/src/com/android/tv/settings/system/development/DevelopmentFragment.java
@@ -634,6 +634,11 @@ public class DevelopmentFragment extends SettingsPreferenceFragment
         }
 
         mAudioDebug.cancelRecording();
+
+        if (mUnavailable) {
+            return;
+        }
+
         mConnectivityManager.unregisterNetworkCallback(mNetworkCallback);
     }
 
